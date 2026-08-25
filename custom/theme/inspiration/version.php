dce3d8d77a92e3fc5dda1e4d59ff393e633b6502
Khoren7788 <khoren.boyakhchyan@gmail.com>
2026-08-25 20:16:46 +0400

    Replace Live Course polling with WebSockets

--- custom/theme/inspiration/version.php
+++ custom/theme/inspiration/version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_inspiration';
-$plugin->version   = 2026082501;
+$plugin->version   = 2026082502;
 $plugin->requires  = 2025100600;
 $plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '0.2.0';
+$plugin->release   = '0.2.1';
